ef0043efe14a4eabdf719479ec54f72c87d18715
zomco <zomcoo@gmail.com>
2022-09-20 17:55:24 +0800

    <feature>(banner): global banner

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  name: "drivercase-web",
   script: "serve",
   env: {
     PM2_SERVE_PATH: './build',
     PM2_SERVE_PORT: 3002,
     PM2_SERVE_SPA: 'true',
-    PM2_SERVE_HOMEPAGE: 'index.html'
+    PM2_SERVE_HOMEPAGE: '/index.html'
   }
 }
