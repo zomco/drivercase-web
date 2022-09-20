c9709c9efa1d51b92d85197963f2c51e138c0b52
zomco <zomcoo@gmail.com>
2022-09-20 17:44:46 +0800

    <feature>(banner): global banner

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,6 @@ module.exports = {
     PM2_SERVE_PATH: './build',
     PM2_SERVE_PORT: 3002,
     PM2_SERVE_SPA: 'true',
-    PM2_SERVE_HOMEPAGE: './index.html'
+    PM2_SERVE_HOMEPAGE: 'index.html'
   }
 }
